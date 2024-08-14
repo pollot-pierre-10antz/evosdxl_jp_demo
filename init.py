20c3c94c7bce1bfbdd9b6dafebd2e1d4df260c26
pollot-pierre-10antz <pollot.pierre@10antz.co.jp>
2024-08-14 15:07:10 +0900

    init commit

---
+++ init.py
@@ -0,0 +1,9 @@
+import sys
+import os   
+
+
+evosdxl_path = os.path.join(os.path.dirname(__file__), "EvoSDXL-JP-v1")
+
+
+if __name__ == "__main__":
+    os.system(f"{sys.executable} -m pip install ")
---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+
+from init import evosdxl_path
+
+sys.path.append(evosdxl_path)
