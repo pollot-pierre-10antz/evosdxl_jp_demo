b71d2870cc45b233c30f8a3dea3d67fc142022f5
pollot-pierre-10antz <pollot.pierre@10antz.co.jp>
2024-08-15 10:25:52 +0900

    revert init

--- README.md
+++ README.md
@@ -9,15 +9,14 @@ git submodule update --init
 Python環境をインストールしてから次を実行する。
 
 ```sh
-# モデルをダウンロードするために、自分のAPI KEYを記入する必要がある
-huggingface-cli login
-
-# パッケージをインストール
-python init.py [--preinstall_model]
+python init.py
 ```
 
 ## Usage
 
 ```sh 
+# (初期化 ONLY) モデルをダウンロードするために、自分のAPI KEYを記入する必要がある
+huggingface-cli login
+
 python main.py [optional subcommands]
 ```
--- init.py
+++ init.py
@@ -1,14 +1,6 @@
 import sys
 import os
 
-from argparse import ArgumentParser, Namespace
-
-
-def parse_arguments() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument("--preinstall_model", action="store_true")
-    return parser.parse_args()
-
 
 this_dir = os.path.dirname(__file__)
 
@@ -19,10 +11,6 @@ requirements = [
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
     os.system(f"{sys.executable} -m pip install --upgrade pip")
     for path in requirements:
         os.system(f"{sys.executable} -m pip install -r {path}")
-    if args.preinstall_model:
-        from evosdxl.evosdxl_jp_v1 import load_evosdxl_jp
-        load_evosdxl_jp("cpu")
