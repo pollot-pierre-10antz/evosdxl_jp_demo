2dff23718037616a94e7ba1166766d8bf9ce7062
pollot-pierre-10antz <pollot.pierre@10antz.co.jp>
2024-08-15 09:20:54 +0900

    preinstall feature

--- init.py
+++ init.py
@@ -1,6 +1,14 @@
 import sys
 import os
 
+from argparse import ArgumentParser, Namespace
+
+
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("--preinstall_model", action="store_true")
+    return parser.parse_args()
+
 
 this_dir = os.path.dirname(__file__)
 
@@ -11,6 +19,10 @@ requirements = [
 
 
 if __name__ == "__main__":
+    args = parse_arguments()
     os.system(f"{sys.executable} -m pip install --upgrade pip")
     for path in requirements:
         os.system(f"{sys.executable} -m pip install -r {path}")
+    if args.preinstall_model:
+        from evosdxl.evosdxl_jp_v1 import load_evosdxl_jp
+        load_evosdxl_jp("cpu")
